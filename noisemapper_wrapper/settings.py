71a2544ad88975e338505a7cf73be414b9ba3091
Ákos Pap <papkos@gmail.com>
2017-01-17 12:43:59 +0100

    Fix ALLOWED_HOSTS setting (caused error in prod).

--- noisemapper_wrapper/settings.py
+++ noisemapper_wrapper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET', 'super_secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG', 'false') == 'true')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
